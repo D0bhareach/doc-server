e39402ff85eddf51d52daa159ccab0278a423c73
Viacheslav Modestov <viacheslav.modestov@gmail.com>
2026-07-10 20:15:46 +0200

    add one more bin to work with file system crates

---
+++ src/bin/files.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("this is main from files");
+}
+
+/*
+ * 1. My tool need to find all target directories in home dir of the user
+ * 2.  Check if this directories hold doc directory it must to be added to vector
+ * 3. process docs from each directory in the vector
+ *
+ * Also app need to fined toolchain directory and add set value of variable.
+ */
