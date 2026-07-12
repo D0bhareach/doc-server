c632aaaf937942eea5c7afce5e1685596be9b4ca
Viacheslav Modestov <viacheslav.modestov@gmail.com>
2026-07-12 19:47:30 +0200

    working dynamic search, very slowgit s!

--- src/bin/files.rs
+++ src/bin/files.rs
@@ -1,8 +1,7 @@
 use std::env;
 use std::fs::read_dir;
-use std::io::Error;
-use std::path::PathBuf;
-use xdg::{BaseDirectories, BaseDirectoriesError};
+use std::io::{Error, ErrorKind, Result as IO_Result};
+use std::path::{Path, PathBuf};
 
 fn get_toolchain_doc_path() -> Option<PathBuf> {
     let home = env::var("RUSTUP_HOME").ok()?;
@@ -16,29 +15,56 @@ fn get_toolchain_doc_path() -> Option<PathBuf> {
     Some(path)
 }
 
-fn get_docs_path(home: PathBuf) -> Result<Vec<PathBuf>, Error> {
-    let paths = Vec::new();
+fn walk_dir_recursive(home: &Path, targets: &mut Vec<PathBuf>) -> IO_Result<()> {
     let home_iter = read_dir(home)?;
-    Ok(paths)
+    for entry in home_iter {
+        let entry = entry?;
+        let p = entry.path();
+        if p.is_dir() {
+            if p.ends_with("target") {
+                if hold_docs(&p).is_ok() {
+                    targets.push(p);
+                }
+            } else {
+                let _ = walk_dir_recursive(&p, targets)?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn hold_docs(path: &Path) -> IO_Result<()> {
+    let target_iter = read_dir(path)?;
+    for entry in target_iter {
+        let e = entry?;
+        let ep = e.path();
+        if ep.is_dir() && ep.ends_with("doc") {
+            return Ok(());
+        }
+    }
+    Err(Error::new(ErrorKind::Other, ""))
+}
+
+pub fn find_doc_entries(dir: &Path) -> IO_Result<Vec<PathBuf>> {
+    let mut results = Vec::new();
+    walk_dir_recursive(dir, &mut results)?;
+    Ok(results)
 }
 
 fn main() {
     let home_dir = env::home_dir().expect("error get $HOME path");
-    let xdg = BaseDirectories::new().expect("error get BaseDirectories");
-    let config_home_dir = xdg.get_config_home();
-
-    println!("home dir: {}", home_dir.to_string_lossy());
-    println!("config_home: {config_home_dir:#?}");
 
     if let Some(toolchain_doc_path) = get_toolchain_doc_path() {
         println!("toolchain: {toolchain_doc_path:#?}");
     };
+
+    match find_doc_entries(&home_dir) {
+        Ok(v) => println!("find doc paths: {v:#?}"),
+        Err(err) => println!("there was an error during walking the dir, {}", err),
+    }
 }
 
 /*
- * 1. My tool need to find all target directories in home dir of the user
- * 2.  Check if this directories hold doc directory it must to be added to vector
- * 3. process docs from each directory in the vector
- *
- * Also app need to fined toolchain directory and add set value of variable.
+ * working prototype really slow. Plan to use ignore by BuntSushi to use multithreading for finding
+ * my docs
  */
